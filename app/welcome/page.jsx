94b332ac7f9653db76c3c10453e3859ab6f46391
Obsessive-Coder <jaredhuff85@gmail.com>
2024-01-31 14:16:35 -0600

    changed some text

--- app/welcome/page.jsx
+++ app/welcome/page.jsx
@@ -12,7 +12,7 @@ const WelcomePage = () => {
 
   return (
     <section className="p-3 bg-dark text-dark-emphasis rounded">
-        <h1 className="text-center">{isRegisterForm ? 'Create Account' : 'Login'}</h1>
+        <h1 className="text-center">{isRegisterForm ? 'Register' : 'Login'}</h1>
         <AuthenticationForm isRegisterForm={isRegisterForm} toggleIsRegisterForm={toggleIsRegisterForm} />
     </section>
   )
