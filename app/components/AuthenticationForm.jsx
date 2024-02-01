3a5a22a88cf57c704be54f63f065a85b6c7a4a75
Obsessive-Coder <jaredhuff85@gmail.com>
2024-01-31 19:01:47 -0600

    refactor

--- app/components/AuthenticationForm.jsx
+++ app/components/AuthenticationForm.jsx
@@ -12,12 +12,10 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 
-const AuthenticationForm = ({toggleIsRegisterForm, isRegisterForm = false}) => {
+const AuthenticationForm = () => {
   const { Formik } = formik;
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [isRegisterForm, setIsRegisterForm] = useState(false);
+  const toggleIsRegisterForm = () => setIsRegisterForm(!isRegisterForm);
   const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
   const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth);
   const { logInGoogle } = UserAuth();
@@ -32,29 +30,30 @@ const AuthenticationForm = ({toggleIsRegisterForm, isRegisterForm = false}) => {
   const schema = yup.object().shape({
     email: yup.string().required().email(),
     password: passwordSchema,
-    ... isRegisterForm ? {
-      username: yup.string().required().min(3).max(15),
-      passwordConfirm: passwordSchema
-    } : {},
+    ... (isRegisterForm ? {
+      passwordConfirm: yup.string()
+        .oneOf([yup.ref('password'), null], "Passwords do not match")
+        .required('Required'),
+    } : {}),
   });
 
-  const handleEmailLogin = async () => {
-    try {
-        const result = await signInWithEmailAndPassword(email.trim(), password.trim());
-        sessionStorage.setItem('user', true);
-        setEmail('');
-        setPassword('');
-    } catch (error) {
-        console.error(error);
-    }
+const handleEmailLogin = async (formData) => {
+  try {
+    const { email, password } = formData;
+    const result = await signInWithEmailAndPassword(email.trim(), password.trim());
+    sessionStorage.setItem('user', true);
+    console.log('USER: ', result);
+  } catch (error) {
+      console.error(error);
+  }
 };
 
-const handleEmailRegistration = async () => {
+const handleEmailRegistration = async (formData) => {
   try {
+       const { email, password } = formData;
        const result = await createUserWithEmailAndPassword(email.trim(), password.trim());
        sessionStorage.setItem('user', true);
-       setEmail('');
-       setPassword('');
+       console.log('USER: ', result);
   } catch (error) {
       console.error(error);
   }
@@ -68,125 +67,98 @@ const handleEmailRegistration = async () => {
     }
   };
 
-  const handleAuthSubmit = event => {
-    event.preventDefault();
-
+  const handleAuthSubmit = formData => {
     if (isRegisterForm) {
-      handleEmailRegistration();
+      handleEmailRegistration(formData);
     } else {
-      handleEmailLogin();
+      handleEmailLogin(formData);
     }
   };
 
   return (
-    <Formik
-      validationSchema={schema}
-      onSubmit={() => alert('HERE')}
-      onChange={() => console.log('HERE AGAIN')}
-      initialValues={{
-        username: '',
-        email: '',
-        password: ''
-      }}
-    >
-      {({ handleSubmit, handleChange, values, touched, errors }) => (
-        <Form noValidate onSubmit={handleSubmit}>
-          {isRegisterForm && (
-            <Form.Group controlId="formUsername" className="my-3">
-              <FloatingLabel controlId="floatingUsername" label="Username">
+    <section className="p-3 bg-dark text-dark-emphasis rounded">
+      <h3 className="text-center">{isRegisterForm ? 'Register' : 'Login'}</h3>
+      <Formik
+        validationSchema={schema}
+        onSubmit={handleAuthSubmit}
+        onChange={() => console.log('HERE AGAIN')}
+        initialValues={{ email: '', password: '' }}
+      >
+        {({ handleSubmit, handleChange, values, touched, errors }) => (
+          <Form noValidate onSubmit={handleSubmit}>
+            <Form.Group controlId="formEmail" className="my-3">
+              <FloatingLabel controlId="floatingEmail" label="Email">
                 <Form.Control
-                  type="text"
-                  name="username"
-                  placeholder="Enter username"
-                  value={values.username}
-                  // onChange={e => setUsername(e.target.value)}
+                  type="email"
+                  name="email"
+                  placeholder="Enter email"
+                  value={values.email} 
                   onChange={handleChange}
-                  isInvalid={!!errors.username}
+                  isInvalid={!!errors.email}
                   className="bg-dark-subtle"
                 />
 
                 <Form.Control.Feedback type="invalid">
-                  {errors.username}
+                  {errors.email}
                 </Form.Control.Feedback>
               </FloatingLabel>
             </Form.Group>
-          )}
 
-          <Form.Group controlId="formEmail" className="my-3">
-            <FloatingLabel controlId="floatingEmail" label="Email">
-              <Form.Control
-                type="email"
-                name="email"
-                placeholder="Enter email"
-                value={values.email} 
-                // onChange={e => setEmail(e.target.value)}
-                onChange={handleChange}
-                isInvalid={!!errors.email}
-                className="bg-dark-subtle"
-              />
-
-              <Form.Control.Feedback type="invalid">
-                {errors.email}
-              </Form.Control.Feedback>
-            </FloatingLabel>
-          </Form.Group>
-
-          <Form.Group controlId="formPassword" className="my-3">
-            <FloatingLabel controlId="floatingPassword" label="Password">
-              <Form.Control
-                type="password"
-                name="password"
-                placeholder="Enter password"
-                value={values.password}
-                // onChange={e => setPassword(e.target.value)}
-                onChange={handleChange}
-                isInvalid={!!errors.password}
-                className="bg-dark-subtle"
-              />
-
-              <Form.Control.Feedback type="invalid">
-                {errors.password}
-              </Form.Control.Feedback>
-            </FloatingLabel>
-          </Form.Group>
-
-          {isRegisterForm && (
-            <Form.Group controlId="formPasswordConfirm" className="my-3">
-              <FloatingLabel controlId="floatingPasswordConfirm" label="Confirm Password">
+            <Form.Group controlId="formPassword" className="my-3">
+              <FloatingLabel controlId="floatingPassword" label="Password">
                 <Form.Control
                   type="password"
-                  name="passwordConfirm"
-                  placeholder="Confirm password"
-                  value={values.passwordConfirm}
-                  // onChange={e => setPasswordConfirm(e.target.value)}
+                  name="password"
+                  placeholder="Enter password"
+                  value={values.password}
                   onChange={handleChange}
-                  isInvalid={!!errors.passwordConfirm}
+                  isInvalid={!!errors.password}
                   className="bg-dark-subtle"
                 />
 
                 <Form.Control.Feedback type="invalid">
-                  {errors.passwordConfirm}
+                  {errors.password}
                 </Form.Control.Feedback>
               </FloatingLabel>
             </Form.Group>
-          )}
 
-          <div className="d-flex justify-content-between">
-            <Button variant="outline-primary" type="submit">
-              {isRegisterForm ? 'Register' : 'Login'}
+            {isRegisterForm && (
+              <Form.Group controlId="formPasswordConfirm" className="my-3">
+                <FloatingLabel controlId="floatingPasswordConfirm" label="Confirm Password">
+                  <Form.Control
+                    type="password"
+                    name="passwordConfirm"
+                    placeholder="Confirm password"
+                    value={values.passwordConfirm}
+                    onChange={handleChange}
+                    isInvalid={!!errors.passwordConfirm}
+                    className="bg-dark-subtle"
+                  />
+
+                  <Form.Control.Feedback type="invalid">
+                    {errors.passwordConfirm}
+                  </Form.Control.Feedback>
+                </FloatingLabel>
+              </Form.Group>
+            )}
+
+            <div className="d-flex justify-content-between">
+              <Button variant="outline-primary" type="submit">
+                {isRegisterForm ? 'Register' : 'Login'}
+              </Button>
+
+              <Button variant="link" onClick={toggleIsRegisterForm} className="px-0">
+                {isRegisterForm ? 'Login' : 'Register'}
+              </Button>
+            </div>
+
+            <Button variant='link' onClick={handleGoogleLogin} className="d-block mx-auto mt-3">
+              Login With Google
             </Button>
-
-            <Button variant="link" onClick={toggleIsRegisterForm} className="px-0">
-              {isRegisterForm ? 'Login' : 'Register'}
-            </Button>
-          </div>
-
-          <Button variant='link' onClick={handleGoogleLogin} className="d-block mx-auto mt-3">
-            Login With Google
-          </Button>
-        </Form>
-      )}
-    </Formik>
+          </Form>
+        )}
+      </Formik>
+    </section>
   )
 }
 
--- app/welcome/page.jsx
+++ app/welcome/page.jsx
@@ -1,20 +1,15 @@
 'use client'
 
-import React, { useState } from 'react'
+import React from 'react'
 
 // Custom Components.
 import AuthenticationForm from '@/app/components/AuthenticationForm'
 
 const WelcomePage = () => {
-    const [isRegisterForm, setIsRegisterForm] = useState(false);
-
-    const toggleIsRegisterForm = () => setIsRegisterForm(!isRegisterForm);
-
   return (
-    <section className="p-3 bg-dark text-dark-emphasis rounded">
-        <h1 className="text-center">{isRegisterForm ? 'Register' : 'Login'}</h1>
-        <AuthenticationForm isRegisterForm={isRegisterForm} toggleIsRegisterForm={toggleIsRegisterForm} />
-    </section>
+    <>
+      <AuthenticationForm />
+    </>
   )
 }
 
