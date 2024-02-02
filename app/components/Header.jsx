6c3321f224c681436823ce158dbba2f269bbe842
Obsessive-Coder <jaredhuff85@gmail.com>
2024-02-02 12:34:56 -0600

    Move sidebar toggle to sidebar

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,10 +4,8 @@ import React from 'react'
 import Link from 'next/link'
 
 // React Bootstrap Components.
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import { List as ListIcon } from 'react-bootstrap-icons';
 
 const Header = ({ pathname, handleToggleSidebar }) => {
   let authPathname = pathname.split('/')[1]
@@ -18,14 +16,6 @@ const Header = ({ pathname, handleToggleSidebar }) => {
     <Navbar collapseOnSelect  expand={"sm"} className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="/" as={Link}>Budgety</Navbar.Brand>
-
-        <Button
-          variant="outline-primary"
-          onClick={handleToggleSidebar}
-          className="d-lg-none px-0 border-0 text-secondary"
-        >
-          <ListIcon size="28" />
-        </Button>
       </Container>
     </Navbar>
   )
--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -12,6 +12,9 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import {
+    ArrowBarLeft as ArrowBarLeftIcon,
+    ArrowBarRight as ArrowBarRightIcon,
+    List as ListIcon,
     Moon as MoonIcon,
     MoonFill as MoonFillIcon
 } from 'react-bootstrap-icons';
@@ -21,36 +24,52 @@ import ToggleSwitch from './ToggleSwitch';
 
 const Sidebar = ({ isOpen, theme, handleSetTheme, handleToggleSidebar, handleLogOut }) => {
   return (
-    <Navbar expand={isOpen} className="bg-body-tertiary">
-        <Container fluid className="flex-column">
-            <Navbar.Offcanvas
-                id="offcanvasNavbarLabel-expand"
-                aria-labelledby="offcanvasNavbarLabel-expand"
-                placement="start"
-                onHide={handleToggleSidebar}
-            >
-                <Offcanvas.Body>
-                    <Nav className="flex-column justify-content-center flex-grow-1 p-3">
-                        {/* <Nav.Item className="px-2">
-                            <ToggleSwitch labelText={theme} handleOnChange={handleSetTheme}>
-                                {theme === 'dark' ? (
-                                    <MoonFillIcon size="18" />
-                                ) : (
-                                    <MoonIcon size="18" />
-                                )}
-                            </ToggleSwitch>
-                        </Nav.Item> */}
-                        <Link href="/" className='nav-link px-2'>Dashboard</Link>
-                        <Link href="/profile" className='nav-link px-2'>Profile</Link>
-                        <Link href="/about" className='nav-link px-2'>About</Link>
-                        <Button variant="link" className='nav-link px-2 text-start' onClick={handleLogOut}>
-                            Logout
-                        </Button>
-                    </Nav>
-                </Offcanvas.Body>
-            </Navbar.Offcanvas>
-        </Container>
-    </Navbar>
+    <div className="position-relative">
+        <Button
+            variant="outline-primary"
+            onClick={handleToggleSidebar}
+            className="position-absolute start-100 px-0 border-0 text-secondary"
+        >
+            {isOpen ? (
+                <ArrowBarLeftIcon size="28" />
+            ) : (
+                <ArrowBarRightIcon size="28" />
+            )}
+        </Button>
+
+        {isOpen && (
+            <Navbar expand={isOpen} className="bg-body-tertiary align-items-start">
+                <Container fluid className="flex-column">
+                    <Navbar.Offcanvas
+                        id="offcanvasNavbar-expand"
+                        aria-labelledby="offcanvasNavbarLabel-expand"
+                        placement="start"
+                        onHide={handleToggleSidebar}
+                    >
+                        <Offcanvas.Body>
+                            <Nav className="flex-column justify-content-center flex-grow-1 p-3">
+                                {/* <Nav.Item className="px-2">
+                                    <ToggleSwitch labelText={theme} handleOnChange={handleSetTheme}>
+                                        {theme === 'dark' ? (
+                                            <MoonFillIcon size="18" />
+                                        ) : (
+                                            <MoonIcon size="18" />
+                                        )}
+                                    </ToggleSwitch>
+                                </Nav.Item> */}
+                                <Link href="/" className='nav-link px-2'>Dashboard</Link>
+                                <Link href="/profile" className='nav-link px-2'>Profile</Link>
+                                <Link href="/about" className='nav-link px-2'>About</Link>
+                                <Button variant="link" className='nav-link px-2 text-start' onClick={handleLogOut}>
+                                    Logout
+                                </Button>
+                            </Nav>
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                </Container>
+            </Navbar>
+        )}
+    </div>
   )
 }
 
