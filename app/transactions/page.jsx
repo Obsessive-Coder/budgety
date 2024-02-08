3770ea7de89df763248e4c7fecdd0e03624c814c
Obsessive-Coder <jaredhuff85@gmail.com>
2024-02-07 18:07:46 -0600

    added more transaction fields to db

--- app/transactions/page.jsx
+++ app/transactions/page.jsx
@@ -8,18 +8,18 @@ import { addDocument } from '@/app/lib/firebase/firestore';
 
 const TransactionsPage = () => {
   const handleIt = async () => {
-    // await addDocument('transactions', { name: 'Hulu', amount: 20.00, isPaid: false })
-    // const x = await getDocuments('transactions', {name: 'Netflix', amount: 25, isPaid: false});
-    // x.forEach((doc) => {
-    //   // doc.data() is never undefined for query doc snapshots
-    //   console.log(doc.id, " => ", doc.data());
-    // });
+    // const x = addDocument('transactions', {
+    //   name: 'Netflix',
+    //   categoryId: '1',
+    //   amount: 15.72,
+    //   dueDate: '',
+    //   paidDate: '',
+    //   isRecurring: false,
+    //   isPaid: false,
 
-    // const x = await updateDocument('transactions', 'ajwBMOyVHmz7kKe4sFUs', {name: 'Yahoo'})
-
-    // await deleteDocument('transactions', 'ajwBMOyVHmz7kKe4sFUs')
-    // await getDocument('transactions', 'ajwBMOyVHmz7kKe4sFUs ')
+    // })
   };
+
   return (
     <div>
       <h1>Transactions Page</h1>
